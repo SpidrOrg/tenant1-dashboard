476ec220c42b538e9d6a1c4461a16ea8b62999c9
AbhayXebia1 <abhay.kumar@xebia.com>
2023-02-16 18:34:29 +0530

    minor refac

--- src/pages/DemandPlanner/DemandPlanner.vue
+++ src/pages/DemandPlanner/DemandPlanner.vue
@@ -1,12 +1,14 @@
 <script>
 import { GChart }  from 'vue-google-charts'
 import fetchMainDashboardData from "@/api/fetchMainDashboardData";
+import TheHeader from './TheHeader.vue'
 import {data} from "autoprefixer";
 import _ from 'lodash';
 export default {
   name: "DemandPlanner",
   components: {
-    GChart
+    GChart,
+    TheHeader
   },
 
   data(){
@@ -30,6 +32,12 @@ export default {
         //chart: {
         //title: 'Implied Market Share',
         legend: {position: 'bottom'},
+        vAxis: {
+          viewWindow: {
+            max: 40,
+            min: 0,
+          },
+        },
         colors: ['#A5A5A5', '#F8D887']
         //}
       },
@@ -52,13 +60,13 @@ export default {
       //alert(this.activeCard);
     },
     colorBtnFunc(n) {
-      if (n <= -20 || n >= 20) {
+      if(Math.abs(n) >= 20) {
         return '#FF3429B2';
-      } else if (n >= -5 || n <= 5) {
-        return '#04BB46';
-      } else if (-20 <= n <= -6 || 6 <= n <= 20) {
+      }
+      if(Math.abs(n) >= 6) {
         return '#FFC107B2';
       }
+      return '#04BB46';
     }
   },
   watch:{
@@ -101,11 +109,8 @@ export default {
     fourth_quarter(oldValue,newValue){
       this.show_array[3] = (oldValue) ? 'show' : 'hide';
     },
-
   }
 };
-
-
 </script>
 
 <template>
@@ -114,7 +119,6 @@ export default {
     top: 35%;
   z-index: 1000;"/>
   <div class="tw-flex tw-w-full tw-h-8 tw-bg-brand-gray-1">
-
     <div class="tw-flex tw-h-full tw-items-center tw-font-bold">
       Demand Planner Dashboard as of Jan 2023
     </div>
@@ -122,9 +126,13 @@ export default {
       Last Refreshed
     </div>
   </div>
-  <div class="tw-flex tw-w-full tw-flex-auto tw-border-t tw-border-solid tw-border-brand-gray-2" v-if="chartDataLoaded">
-    <div class="tw-flex tw-align-center tw-grid tw-grid-cols-4 tw-gap-2.5 tw-m-2">
-      <div>
+  <div class="tw-flex tw-w-full tw-flex-auto tw-border-t tw-border-solid tw-border-brand-gray-2" />
+  <div class="tw-py-5 tw-bg-brand-gray-1" v-if="chartDataLoaded">
+    <TheHeader />
+  </div>
+  <div class="tw-flex tw-w-full tw-items-center tw-justify-end tw-border-b tw-border-solid tw-border-brand-gray-2" v-if="chartDataLoaded">
+    <p>Show Projections for:</p>
+    <div class="tw-flex">
         <v-checkbox
           label="1-3 months (Default)"
           color="#7823DC"
@@ -132,67 +140,62 @@ export default {
           v-model="first_quarter"
           :disabled=true
         ></v-checkbox>
-      </div>
-      <div>
         <v-checkbox
           label="4-6 months"
           color="#7823DC"
           hide-details
           v-model="second_quarter"
         ></v-checkbox>
-      </div>
-      <div>
         <v-checkbox
           label="6-9 months"
           color="#7823DC"
           hide-details
           v-model="third_quarter"
         ></v-checkbox>
-      </div>
-      <div>
         <v-checkbox
           label="10-12 months"
           color="#7823DC"
           hide-details
           v-model="fourth_quarter"
         ></v-checkbox>
-      </div>
+    </div>
+    <div class="tw-flex tw-pl-24 tw-pr-4">
+      <v-btn
+        :rounded="0"
+        color="#7823DC"
+      >
+        Switch to Fixed View
+      </v-btn>
     </div>
   </div>
-  <div class="tw-flex tw-w-full tw-flex-auto tw-border-t tw-border-solid tw-border-brand-gray-2" v-if="chartDataLoaded">
+  <div class="tw-flex tw-w-full tw-flex-auto tw-py-5" v-if="chartDataLoaded">
       <div class="tw-grid tw-grid-cols-4 tw-gap-2.5 tw-m-2" v-if="chartDataLoaded && apiData.length">
         <v-card v-for="(data, index) in apiData" :key="data.period" @click="activeEl(index)" :style="(activeCard == index) ? 'border:1px solid #7823DC': '' ">
           <v-container>
-            <div class="tw-grid tw-grid-cols-2 tw-mb-2">
-              <div>
-                <p class="projectedPeriod">Projected Period</p>
-                <h3 class="periodDates">{{ data.period }}</h3>
-              </div>
-              <div style="display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-padding: 5px;
-background: #EDEDED;
-border-radius: 4px;">
-                <span class="monthsLag">{{ data.lag }} Months Lag</span>
-              </div>
-              <v-divider/>
+            <div class="tw-flex tw-flex-col tw-pb-2">
+                <p style="color: #9291A5;">Projected Period</p>
+                <div class="tw-flex tw-gap-x-4 tw-items-center tw-w-full">
+                  <p class="tw-text-xl tw-font-medium">{{ data.period }}</p>
+                  <div class="tw-bg-brand-gray-4 tw-rounded">
+                    <p class="tw-p-1">{{ data.lag }} Months Lag</p>
+                  </div>
+                </div>
             </div>
-            <div class="tw-grid tw-grid-cols-2">
+            <div class="tw-w-full tw-border-t tw-border-solid tw-border-brand-gray-2" v-if="chartDataLoaded"/>
+            <div class="tw-grid tw-grid-cols-2 tw-pt-2">
               <div>
-                <h4 class="tw-font-bold" style="font-size: 15px">Projected Growth (%YoY)</h4>
-                <h2 class="varianceText" :style="{'color': colorBtnFunc(data.variance)}">{{ `${data.variance}%` }}</h2>
-                <span style="font-size: 10px">Variance</span>
-                <v-divider/>
+                <h4 class="tw-font-bold tw-text-sm">Projected Growth (%YoY)</h4>
+                <h2 class="tw-text-4xl tw-font-semibold" :style="{'color': colorBtnFunc(data.variance)}">{{ `${data.variance}%` }}</h2>
+                <span class="tw-text-l">Variance</span>
+                <div class="tw-w-full tw-border-t tw-border-solid tw-border-brand-gray-2" v-if="chartDataLoaded"/>
                 <div class="tw-grid tw-grid-cols-2 tw-mt-3">
                   <div>
-                    <h4 class="headings">{{ `${data.marketSensing}%` }}</h4>
-                    <span style="font-size: 10px">Market Sensing</span>
+                    <p class="tw-text-2xl tw-font-semibold">{{ `${data.marketSensing}%` }}</p>
+                    <span class="tw-text-xs">Market Sensing</span>
                   </div>
                   <div>
-                    <h4 class="headings">{{ `${data.internal}%` }}</h4>
-                    <span style="font-size: 10px">Internal Forecast</span>
+                    <p class="tw-text-2xl tw-font-semibold">{{ `${data.internal}%` }}</p>
+                    <span class="tw-text-xs">Internal Forecast</span>
                   </div>
                 </div>
               </div>
@@ -240,13 +243,8 @@ border-radius: 4px;">
         </v-card>
       </div>
   </div>
-
-  <h3 class="tw-p-2 tw-mt-4 tw-mb-4 tw-font-bold" style="font-style: normal;
-font-weight: 500;
-font-size: 24px;
-line-height: 32px;" v-if="chartDataLoaded">More details for {{projectionMonth}} </h3>
-  <v-divider class="tw-mt-2 tw-mb-3"/>
-    <div class="tw-mt-4 tw-w-full tw-border-t tw-border-solid tw-border-brand-gray-2" v-if="chartDataLoaded">
+  <h3 class="tw-pt-4 tw-pb-2 tw-font-medium tw-text-2xl" v-if="chartDataLoaded">More details for {{projectionMonth}} </h3>
+  <div class="tw-py-4 tw-w-full tw-border-t tw-border-solid tw-border-brand-gray-2" v-if="chartDataLoaded">
       <div class="tw-grid tw-grid-cols-4 tw-gap-3">
         <div class="tw-col-span-1">
           <v-card class="tw-shadow-none">
@@ -275,67 +273,10 @@ line-height: 32px;" v-if="chartDataLoaded">More details for {{projectionMonth}} 
               type="ColumnChart"
               :data="columnChartData"
               :options="columnChartOptions"
-
             />
           </v-card>
         </div>
 
       </div>
-    </div>
+  </div>
 </template>
-<style scoped>
-.projectedPeriod {
-  font-style: normal;
-  font-weight: 400;
-  font-size: 15px;
-  line-height: 20px;
-  color: #9291A5;
-}
-.periodDates {
-  font-style: normal;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 27px;
-  /* identical to box height */
-
-  display: flex;
-  align-items: center;
-
-  /* Neutral Colors/Black */
-
-  color: #1E1B39;
-}
-
-.monthsLag {
-font-style: normal;
-  font-weight: 400;
-  font-size: 13px;
-  line-height: 14px;
-  /* identical to box height, or 93% */
-
-
-  color: #000000;
-
-
-  /* Inside auto layout */
-
-  flex: none;
-  order: 0;
-  flex-grow: 0;
-}
-
-.varianceText {
-  font-style: normal;
-  font-weight: 600;
-  font-size: 40px;
-  line-height: 48px;
-}
-
-.headings {
-  font-family: 'Graphik';
-  font-style: normal;
-  font-weight: 600;
-  font-size: 20px;
-  line-height: 27px;
-}
-</style>

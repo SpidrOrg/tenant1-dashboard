561801f2b88f19ee65c5c2ddf1ac767a830ce004
Shivang Trivedi <Shivang.Trivedi@XEBIAINDIA.LOCAL>
2023-02-20 16:25:08 +0530

    Internal charts progress

---
+++ src/pages/InternalCharts/TheHeader.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "TheHeader",
+  data(){
+    return {
+      isByVolume: false,
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="tw-flex tw-gap-x-4 tw-w-full tw-bg-white tw-px-3">
+    <div class="tw-pt-5 tw-w-1/6">
+      <v-select
+        label="All Categories"
+        :items="['All', 'Shirts', 'Pants', 'Jackets']"
+      ></v-select>
+    </div>
+    <div class="tw-pt-5 tw-w-1/6">
+      <v-select
+        label="All Customers"
+        :items="['All', 'Amazon', 'Walmart']"
+      ></v-select>
+    </div>
+    <div class="tw-pt-5 tw-w-1/6">
+      <v-select
+        label="Geography"
+        :items="['USA']"
+      ></v-select>
+    </div>
+    <div class="tw-flex tw-items-center tw-gap-1.5 tw-pl-2">
+      <p :class="`${!isByVolume ? 'tw-font-medium' : ''}`">Value (USD)</p>
+      <div class="tw-flex tw-pt-5">
+        <v-switch label="Volume" inset @click="isByVolume = !isByVolume"></v-switch>
+      </div>
+    </div>
+  </div>
+</template>

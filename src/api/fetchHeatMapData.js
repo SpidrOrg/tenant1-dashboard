de7c3c415de06235b1521cae0f2d2ba623a3ba2a
AbhayXebia1 <abhay.kumar@xebia.com>
2023-03-13 15:59:57 +0530

    heat map page code refactor

---
+++ src/api/fetchHeatMapData.js
@@ -0,0 +1,91 @@
+import { invokeApi } from '@/parent/idpUtils';
+
+let cachedData = [
+  {
+    period: 'Mar 23 - May 23',
+    columnHeaders: [
+      'Name',
+      'Category1',
+      'Category2',
+      'Category3',
+      'Category4',
+      'Category5',
+      'Category6',
+      'Category7',
+    ],
+    records: [
+      ['Value(USD)', 14, 14, 23, 32, 67, 98, 3],
+      ['Customer1', 4, 54, 3, 52, 6, 98, 34],
+      ['Customer2', 44, 24, 83, 62, 7, 38, 23],
+      ['Customer3', 54, 84, 53, 72, 67, 8, 63],
+    ],
+  },
+  {
+    period: 'Jun 23 - Aug 23',
+    columnHeaders: [
+      'Name',
+      'Category1',
+      'Category2',
+      'Category3',
+      'Category4',
+      'Category5',
+      'Category6',
+      'Category7',
+    ],
+    records: [
+      ['Value(USD)', 14, 14, 23, 32, 67, 98, 3],
+      ['Customer1', 4, 54, 3, 52, 6, 98, 34],
+      ['Customer2', 44, 24, 83, 62, 7, 38, 23],
+      ['Customer3', 54, 84, 53, 72, 67, 8, 63],
+    ],
+  },
+  {
+    period: 'Sep 23 - Nov 23',
+    columnHeaders: [
+      'Name',
+      'Category1',
+      'Category2',
+      'Category3',
+      'Category4',
+      'Category5',
+      'Category6',
+      'Category7',
+    ],
+    records: [
+      ['Value(USD)', 14, 14, 23, 32, 67, 98, 3],
+      ['Customer1', 4, 54, 3, 52, 6, 98, 34],
+      ['Customer2', 44, 24, 83, 62, 7, 38, 23],
+      ['Customer3', 54, 84, 53, 72, 67, 8, 63],
+    ],
+  },
+  {
+    period: 'Dec 23 - Feb 24',
+    columnHeaders: [
+      'Name',
+      'Category1',
+      'Category2',
+      'Category3',
+      'Category4',
+      'Category5',
+      'Category6',
+      'Category7',
+    ],
+    records: [
+      ['Value(USD)', 14, 14, 23, 32, 67, 98, 3],
+      ['Customer1', 4, 54, 3, 52, 6, 98, 34],
+      ['Customer2', 44, 24, 83, 62, 7, 38, 23],
+      ['Customer3', 54, 84, 53, 72, 67, 8, 63],
+    ],
+  },
+];
+export default async function () {
+  if (cachedData) {
+    return await new Promise((res) => {
+      setTimeout(() => {
+        res(cachedData);
+      }, 1300);
+    });
+  }
+  const data = await invokeApi('heatmap');
+  return data;
+}
---
+++ src/pages/HeatMap/CardsListItem.vue
@@ -0,0 +1,77 @@
+<script>
+import TooltipChart from '@/pages/HeatMap/TooltipChart.vue';
+
+export default {
+  name: 'CardsListItem',
+  props: {
+    data: {
+      type: Object,
+      required: true,
+    },
+  },
+  components: {
+    TooltipChart,
+  },
+  methods: {
+    getCellStyling(cellValue) {
+      let styles = 'tw-cursor-pointer tw-text-center tw-rounded';
+
+      if (Math.abs(cellValue) >= 20) styles += ' tw-bg-brand-red-1';
+      else if (Math.abs(cellValue) >= 6) styles += ' tw-bg-brand-yellow-1';
+      else styles += ' tw-bg-brand-green-2';
+
+      return styles;
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    class="tw-flex tw-flex-col tw-gap-3 tw-w-full tw-h-full tw-bg-white tw-p-4"
+  >
+    <p class="tw-text-xl tw-font-medium">{{ data.period }}</p>
+    <div class="tw-w-full tw-border tw-border-solid tw-border-brand-gray-2" />
+    <div class="tw-w-full tw-h-full tw-overflow-auto">
+      <div
+        :class="`tw-grid tw-grid-cols-${data.columnHeaders.length} tw-gap-x-3 tw-pb-4`"
+      >
+        <span
+          v-for="columnHeader in data.columnHeaders"
+          :key="columnHeader"
+          class="tw-font-medium tw-text-sm"
+        >
+          {{ columnHeader }}
+        </span>
+      </div>
+      <div :class="`tw-grid tw-grid-rows-${data.records.length} tw-gap-y-3`">
+        <div
+          v-for="rowData in data.records"
+          :key="rowData[0]"
+          :class="`tw-grid tw-grid-cols-${rowData.length} tw-gap-x-3 tw-items-center`"
+        >
+          <div
+            v-for="(cellValue, index) in rowData"
+            :key="`${rowData[0]}-${cellValue}`"
+          >
+            <v-menu location="right">
+              <template v-slot:activator="{ props }">
+                <div
+                  v-bind="index > 0 && props"
+                  :class="`tw-py-3 tw-font-medium ${
+                    index > 0 ? getCellStyling(cellValue) : 'tw-text-sm'
+                  }`"
+                >
+                  {{ `${cellValue}${index > 0 ? '%' : ''}` }}
+                </div>
+              </template>
+              <div class="tw-border-2 tw-shadow-2xl tw-p-0">
+                <TooltipChart />
+              </div>
+            </v-menu>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>

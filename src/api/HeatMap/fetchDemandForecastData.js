60aa07c5ba3da2411374d2fa7bc4216ea71edafe
visdxebia <vishal.daga@xebia.com>
2023-04-13 19:20:44 +0530

    wirign with api data from heat map historical

--- src/api/HeatMap/fetchDemandForecastData.js
+++ src/api/HeatMap/fetchDemandForecastData.js
@@ -10,19 +10,19 @@ const getNumericValue = (value) => {
   return _.toNumber(value);
 };
 
-let cachedData = [
-  ['Jun 22 - Aug 22', 12, 33, 44],
-  ['Aug 22 - Oct 22', 55, 70, 40],
-  ['Sep 22 - Nov 22', 13, 66, 20],
-  ['Oct 22 - Dec 22', 24, 30, 54],
-  ['Nov 22 - Jan 23', 55, 70, 40],
-  ['Dec 22 - Feb 23', 13, 66, 20],
-  ['Jan 23 - Mar 23', 24, 10, 40],
-  ['Feb 23 - Apr 23', 55, 11, 4],
-  ['Mar 23 - May 23', 13, 60, 90],
-  ['Apr 23 - Jun 23', 24, 30, 50],
-  ['May 23 - Jul 23', 13, 10, 4],
-];
+// let cachedData = [
+//   ['Jun 22 - Aug 22', 12, 33, 44],
+//   ['Aug 22 - Oct 22', 55, 70, 40],
+//   ['Sep 22 - Nov 22', 13, 66, 20],
+//   ['Oct 22 - Dec 22', 24, 30, 54],
+//   ['Nov 22 - Jan 23', 55, 70, 40],
+//   ['Dec 22 - Feb 23', 13, 66, 20],
+//   ['Jan 23 - Mar 23', 24, 10, 40],
+//   ['Feb 23 - Apr 23', 55, 11, 4],
+//   ['Mar 23 - May 23', 13, 60, 90],
+//   ['Apr 23 - Jun 23', 24, 30, 50],
+//   ['May 23 - Jul 23', 13, 10, 4],
+// ];
 
 export default async function ({
   marketSensingRefreshDate,
@@ -31,13 +31,13 @@ export default async function ({
   customer,
   lag,
 }) {
-  if (cachedData) {
-    return await new Promise((res) => {
-      setTimeout(() => {
-        res(cachedData);
-      }, 1300);
-    });
-  }
+  // if (cachedData) {
+  //   return await new Promise((res) => {
+  //     setTimeout(() => {
+  //       res(cachedData);
+  //     }, 1300);
+  //   });
+  // }
 
   const data = await getApiBase('heat_map_historical', {
     marketSensingRefreshDate,

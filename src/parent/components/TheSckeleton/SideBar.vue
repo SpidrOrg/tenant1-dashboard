86e629db35254f4dc8465f9e5b8ba13a95845845
AbhayXebia1 <abhay.kumar@xebia.com>
2023-04-20 17:37:02 +0530

    common parent container fixes

--- src/parent/components/TheSckeleton/SideBar.vue
+++ src/parent/components/TheSckeleton/SideBar.vue
@@ -1,54 +1,66 @@
 <script>
 import _ from 'lodash';
 
-const LANDING_ROUTE_NAME = "landing";
+const LANDING_ROUTE_NAME = 'landing';
 
 export default {
-  name: "SideBar",
+  name: 'SideBar',
   props: {
     orgLogo: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
-  data(){
+  data() {
     return {
-      menuItems: []
-    }
+      menuItems: [],
+    };
   },
   computed: {
-    cRoute(){
-      if (!this.$route.name || this.$route.name === LANDING_ROUTE_NAME){
-        return _.get(this.$router, "options.routes[0].children[1].name", "");
+    cRoute() {
+      if (!this.$route.name || this.$route.name === LANDING_ROUTE_NAME) {
+        return _.get(this.$router, 'options.routes[0].children[1].name', '');
       }
-      return this.$route.name
-    }
+      return this.$route.name;
+    },
   },
   mounted() {
-    const allRouteItems = _.map(_.get(this.$router, "options.routes[0].children", []), v => _.omit(v, "component"));
-    this.menuItems = _.filter(allRouteItems, v => v.name !== LANDING_ROUTE_NAME)
+    const allRouteItems = _.map(
+      _.get(this.$router, 'options.routes[0].children', []),
+      (v) => _.omit(v, 'component')
+    );
+    this.menuItems = _.filter(
+      allRouteItems,
+      (v) => v.name !== LANDING_ROUTE_NAME
+    );
   },
-}
+};
 </script>
 
 <template>
   <div class="logo-area">
     <div class="logo">
-      <img class="logoImage" :src="orgLogo"/>
+      <img class="logoImage" :src="orgLogo" />
     </div>
   </div>
   <div class="menu-area">
-    <router-link v-for="menuItem in menuItems" :key="menuItem.name" :to="menuItem.path" :class="`menu-item ${menuItem.name === cRoute ? 'menu-item-selected': ''}`">
+    <router-link
+      v-for="menuItem in menuItems"
+      :key="menuItem.name"
+      :to="menuItem.path"
+      :class="`menu-item ${
+        menuItem.name === cRoute ? 'menu-item-selected' : ''
+      }`"
+    >
       <div class="menu-item-content">
         <div class="menu-item-content-image">
-          <img :src="menuItem.icon_active" v-if="menuItem.name === cRoute"/>
-          <img :src="menuItem.icon" v-else/>
+          <img :src="menuItem.icon_active" v-if="menuItem.name === cRoute" />
+          <img :src="menuItem.icon" v-else />
         </div>
         <div>
-          {{menuItem.label}}
+          {{ menuItem.label }}
         </div>
       </div>
-
     </router-link>
   </div>
 </template>
@@ -56,7 +68,7 @@ export default {
 <style scoped>
 .logo-area {
   width: 100%;
-  height: 4rem;
+  height: 88px;
   display: flex;
   align-self: center;
   justify-self: center;
@@ -64,10 +76,13 @@ export default {
 .logo {
   display: flex;
   object-fit: contain;
-  margin: auto;
+  margin-top: 16px;
+  margin-right: auto;
+  margin-bottom: auto;
+  margin-left: auto;
 }
 .logoImage {
-  width: 125px;
+  width: 128px;
 }
 .menu-area {
   display: flex;
@@ -78,25 +93,25 @@ export default {
   display: flex;
   width: 100%;
   height: 48px;
-  background: #FFFFFF;
+  background: #ffffff;
   padding: 24px 16px;
   align-items: center;
   flex-wrap: nowrap;
-  color: #1E1E1E;
+  color: #1e1e1e;
   cursor: pointer;
 }
 .menu-item-content {
   display: flex;
-  gap:8px;
+  gap: 8px;
 }
 
 .menu-item-content-image {
   width: 25px;
-  height: 19px
+  height: 19px;
 }
 
 .menu-item-selected {
-  background: #8737E1;
-  color: #FFFFFF;
+  background: #8737e1;
+  color: #ffffff;
 }
 </style>
